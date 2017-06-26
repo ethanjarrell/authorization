216536735fd56a025c346d28f5ee7d93b8d36768
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-26 14:52:15 -0400

    this is my first commit

---
+++ userData.js
@@ -0,0 +1,18 @@
+
+
+module.exports = {
+  users: [
+    {
+      username: "amy",
+      password: "chihuahua"
+    },
+    {
+      username: "grayson",
+      password: "dinotrux"
+    },
+    {
+      username: "lilamae",
+      password: "aye-aye"
+    }
+  ]
+}
